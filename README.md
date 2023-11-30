398ac4758a59fddbdc6cf0f221bcc5c5b1955f4e
Rishabh Jain <rishabh20000jain@gmail.com>
2023-11-30 10:27:59 +0530

    removed keys from aws file

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Polaris Grid
 1. Check your flutter version it should be Flutter 3.13.6 and Dart 3.1.3 or more.
 2. flutter pub get
 3. flutter pub run build_runner build --delete-conflicting-outputs
-4. Execute step 2 and step 3 command this install all the dependencies and will generate all the required code that has to be generated. 
+4. Add your aws keys in common/aws_common.dart
+5. Execute step 2 and step 3 command this install all the dependencies and will generate all the required code that has to be generated. 
 
--- lib/common/aws_common.dart
+++ lib/common/aws_common.dart
@@ -2,9 +2,8 @@ String buildFileUrl(String filePathOnBucket, String fileName) =>
     'https://${AWSConstants.bucketName}.${AWSConstants.s3Service}.${AWSConstants.region}.amazonaws.com/$filePathOnBucket/$fileName';
 
 abstract class AWSConstants {
-  static const String accessKeyId = 'AKIARUYJYFCSRJUWGKQY';
-  static const String secretAccessKey =
-      '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';
+  static const String accessKeyId = '';
+  static const String secretAccessKey = '';
   static const String region = 'ap-south-1';
   static const String bucketName = 'assignments-list';
   static const String s3Service = 's3';
